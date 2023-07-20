f123d8fe0e06d9e0a374b50de6502cf81e4bca27
andrey91.workstation@icloud.com <andrey91.workstation@icloud.com>
2023-07-20 12:25:43 +0300

    Deploy app to GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/rest-app/" : "/",
+  outputDir: "public", // Указываем путь сборки, чтобы приложение сохранялось в папку public
   transpileDependencies: ["vue"],
 };
